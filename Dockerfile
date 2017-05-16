b2d15204a1266f50dbb6ab74e98b445e1e3b90e7
Pratik Parikh <Pratik Parikh>
2017-05-16 10:51:37 -0400

    updated.

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM alpine:latest
 
+RUN apk add dos2unix --update-cache --repository http://dl-3.alpinelinux.org/alpine/edge/testing/ --allow-untrusted
+
+RUN apk --update add bash && apk --no-cache add wget curl
+
 RUN apk add --update ca-certificates openssl tar && \
     wget https://github.com/coreos/etcd/releases/download/v3.2.0-rc.0/etcd-v3.2.0-rc.0-linux-amd64.tar.gz && \
     tar zxvf etcd-v3.2.0-rc.0-linux-amd64.tar.gz && \
@@ -11,4 +15,5 @@ VOLUME /data
 EXPOSE 2379 2380 4001 7001
 ADD run.sh /bin/run.sh
 RUN chmod -R 755 /bin/run.sh
+RUN dos2unix /bin/run.sh
 ENTRYPOINT ["/bin/run.sh"]
--- run.sh
+++ run.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
+set -e
+
+IFACE=${1:-eth0}
+
+IP=`ifconfig $IFACE | grep "inet addr" | cut -d ':' -f 2 | cut -d ' ' -f 1`
+URL="http://${IP}"
+
+echo ${URL}
+
 # check for $CLIENT_URLS
 if [ -z ${CLIENT_URLS+x} ]; then
-    CLIENT_URLS="http://0.0.0.0:4001,http://0.0.0.0:2379"
+    CLIENT_URLS="${URL}:4001,${URL}:2379"
     echo "Using default CLIENT_URLS ($CLIENT_URLS)"
 else
     echo "Detected new CLIENT_URLS value of $CLIENT_URLS"
@@ -11,13 +20,13 @@ fi
 # check for $PEER_URLS
 
 if [ -z ${PEER_URLS+x} ]; then
-    PEER_URLS="http://0.0.0.0:7001,http://0.0.0.0:2380"
+    PEER_URLS="${URL}:7001,${URL}:2380"
     echo "Using default PEER_URLS ($PEER_URLS)"
 else
     echo "Detected new PEER_URLS value of $PEER_URLS"
 fi
 
-ETCD_CMD="/bin/etcd -data-dir=/data -listen-peer-urls=${PEER_URLS} -listen-client-urls=${CLIENT_URLS} $*"
+ETCD_CMD="/bin/etcd -data-dir=/data -listen-peer-urls=${PEER_URLS} -listen-client-urls=${CLIENT_URLS} --advertise-client-urls=${CLIENT_URLS},http://127.0.0.1:2379,http://127.0.0.1:4001 $*"
 echo -e "Running '$ETCD_CMD'\nBEGIN ETCD OUTPUT\n"
 
 exec $ETCD_CMD
